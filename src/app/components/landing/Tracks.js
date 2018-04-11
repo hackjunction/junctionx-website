a1a41d622efb2024a6e4c0abe4f707a9d5743a25
aajuha <aaro.hameri@gmail.com>
2018-04-11 22:13:20 +0300

    relevant changes to tracks.js and some stylesheets

--- src/app/components/landing/Tracks.js
+++ src/app/components/landing/Tracks.js
@@ -1,43 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
 import Scroll from 'react-scroll';
-import ElementGrid from '../common/ElementGrid';
-import TrackElement from '../common/TrackElement';
 import Block from '../viewBlocks/Block';
 import BlockHeader from '../viewBlocks/BlockHeader';
-
 import styles from './Tracks.c.scss';
 
-class Tracks extends Component {
+class Tracks extends React.Component {
   componentWillMount() {
     // the first time we load the app, we need that tracks list
     this.props.dispatch({ type: 'GET_TRACKS' });
   }
   render() {
-    var grid = [[4, 4, 4], [6, 6], [4, 4, 4], [6, 6], [4, 4, 4]];
-    var i = 0;
-
     return (
       <Scroll.Element name="tracks">
         <Block className={styles.block}>
-          <Row>
-            <Col xs={12} className="bold text-secondary">
-              <h4>TRACKS FROM LAST YEAR</h4>
+          <BlockHeader title="TRACKS"/>
+          <Row className="junction_tracks" center="xs">
+            {/* Tähän kohtaan fetchatusta datasta trackit täyttämään */ }
+            <Col xs={12} md={6} className="trackbox">
+              <img
+                src="https://staging.hackjunction.com/wp-content/uploads/2017/08/elisa.png"
+                className="responsive"
+                alt=""
+              />
             </Col>
           </Row>
-          <BlockHeader title="BUILDING FOR TOMORROW">
-            <p>
-              Junction 2017 was divided into multiple tracks, each including several challenges set by our amazing
-              partners. The best project was awarded with the main prize of{' '}
-              <span className={styles.text_highlight}>20k€</span>.
-            </p>
-          </BlockHeader>
-          <ElementGrid
-            grid={[[4, 4, 4], [6, 6], [4, 4, 4], [6, 6], [4, 4, 4]]}
-            elements={this.props.tracks.map((track, i) => <TrackElement key={i} post={track} />)}
-          />
         </Block>
       </Scroll.Element>
     );
