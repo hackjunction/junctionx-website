200a4b09c49ca72fd5fd5576089acd9b47b8b5d6
Henrik Aarnio <hjaarnio@gmail.com>
2018-04-21 00:06:03 +0300

    adjust footer

--- src/app/components/Landing/Footer.js
+++ src/app/components/Landing/Footer.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Row, Col } from 'react-flexbox-grid';
+import PropTypes from 'prop-types';
 import Block from '../viewBlocks/Block';
 import styles from './Footer.c.scss';
-import PropTypes from 'prop-types';
 
 const Footer = props => (
   <Block className={styles.block} contentClassName={styles.blockContent} noSeparator>
     <Row center="xs">
       <Col xs={12} md={4}>
-        <h3>CONTACT:</h3>
+        {props.email && <h3>CONTACT:</h3>}
         <p>{props.email}</p>
+        <a className={styles.a} href="https://www.hackjunction.com/">
+          JUNCTION
+        </a>
       </Col>
     </Row>
   </Block>
 );
+Footer.propTypes = {
+  email: PropTypes.string
+};
 
 export default Footer;
