6b985989097bc4db3045c50aabcbd9e058fdb6c7
Henrik Aarnio <hjaarnio@gmail.com>
2018-04-24 17:08:48 +0300

    add links to track pages, and the track pages

--- src/app/components/Landing/Tracks.js
+++ src/app/components/Landing/Tracks.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
 import Scroll from 'react-scroll';
+import { Link } from 'react-router-dom';
 import Block from '../viewBlocks/Block';
 import BlockHeader from '../viewBlocks/BlockHeader';
 import styles from './Tracks.c.scss';
@@ -20,11 +21,13 @@ class Tracks extends React.Component {
           <Row className={styles.trackDivs} center="xs">
             {this.props.tracks.map((track, i) => (
               <Col xs={12} sm={8} md={6} key={i} className={styles.trackbox}>
-                <div className={styles.imgTrack} style={{ background: 'url(' + track.image + ')' }}>
-                  <div className={styles.transpr}>
-                    <h2 className={styles.title1}>{track.title.toUpperCase()}</h2>
+                <Link to={`/tracks/${track.slug}`}>
+                  <div className={styles.imgTrack} style={{ background: 'url(' + track.image + ')' }}>
+                    <div className={styles.transpr}>
+                      <h2 className={styles.title1}>{track.title.toUpperCase()}</h2>
+                    </div>
                   </div>
-                </div>
+                </Link>
               </Col>
             ))}
           </Row>
--- src/app/components/Track.js
+++ src/app/components/Track.js
@@ -1,9 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import styles from './Track.c.scss';
+import Header from './Landing/Header';
+import Footer from './Landing/Footer';
 
-class Track extends React.Component {
+class Track extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  componentWillMount() {
+    if (this.props.tracks.length == 0) {
+      this.props.getTracks();
+    }
+    if (!this.props.event) this.props.getEvent();
+  }
+
   render() {
-    return <div>Yay</div>;
+    const match = this.props.match;
+    if (!match.params || !match.params.track) return <Redirect to="/" />;
+    if (this.props.tracks.length > 0) {
+      var track = this.props.tracks.filter(track => track.slug === match.params.track);
+      if (track.length === 0) return <Redirect to="/404" />;
+      else track = track[0];
+    } else track = {};
+
+    var style = {
+      backgroundImage: `url(${track.image})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover'
+    };
+
+    const e = this.props.event;
+    return (
+      <div className={styles.track_page}>
+        <Grid className={styles.grid} fluid>
+          <Row style={style} className={styles.background_top}>
+            <Col className={styles.track_name} xs={12} sm={12} md={12} lg={12}>
+              <Header name={e && e.name} />
+            </Col>
+          </Row>
+          <Row center="xs">
+            <Col xs={12} sm={12} md={12}>
+              <h1>{track.title}</h1>
+            </Col>
+            <Col xs={12} sm={12} md={12}>
+              <div dangerouslySetInnerHTML={{ __html: track.content }} />
+            </Col>
+          </Row>
+          <hr className={styles.separator} />
+        </Grid>
+        <Row center="xs">
+          <Footer email={e && e.email} />
+        </Row>
+      </div>
+    );
   }
 }
 
-export default Track;
+Track.propTypes = {
+  tracks: PropTypes.array,
+  getTracks: PropTypes.func,
+  getEvent: PropTypes.func,
+  match: PropTypes.object,
+  event: PropTypes.object
+};
+
+function mapStateToProps(state) {
+  //console.log('tracks', state.tracks);
+  return {
+    tracks: state.tracks || [],
+    event: state.event
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getTracks() {
+      dispatch({ type: 'GET_TRACKS' });
+    },
+    getEvent() {
+      dispatch({ type: 'GET_EVENT' });
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Track);
